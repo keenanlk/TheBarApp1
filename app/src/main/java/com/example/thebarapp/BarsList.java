de2c20db49dfd7ada30fa528ad11b31a1df82852
keenanlk <keenanlkaufman@gmail.com>
2019-11-14 16:42:06 -0600

    Design changes.

--- app/src/main/java/com/example/thebarapp/BarsList.java
+++ app/src/main/java/com/example/thebarapp/BarsList.java
@@ -14,12 +14,33 @@ public class BarsList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bars_list);
 
+        //Click to get to Old Broadway activity
         CardView car = (CardView) findViewById(R.id.card1);
         car.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                  Intent obIntent = new Intent(getApplicationContext(), OldBroadway.class);
                  startActivity(obIntent);
+            }
+        });
+
+        //Click to get to Sports Bar Activity
+        CardView sports = (CardView) findViewById(R.id.sportsBarCard);
+        sports.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent obIntent = new Intent(getApplicationContext(), SportsBar.class);
+                startActivity(obIntent);
+            }
+        });
+
+        //Click to get to HerdAndHorns activity
+        CardView herd = (CardView) findViewById(R.id.HerdHornsCard);
+        herd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent obIntent = new Intent(getApplicationContext(), HerdAndHorns.class);
+                startActivity(obIntent);
             }
         });
     }
---
+++ app/src/main/java/com/example/thebarapp/HerdAndHorns.java
@@ -0,0 +1,14 @@
+package com.example.thebarapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HerdAndHorns extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_herd_and_horns);
+    }
+}
