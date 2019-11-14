f75a59ce28d951f823b43dc7625cc28eca3c8c50
keenanlk <keenanlkaufman@gmail.com>
2019-11-14 14:42:14 -0600

    Made card clickable.

--- app/src/main/java/com/example/thebarapp/BarsList.java
+++ app/src/main/java/com/example/thebarapp/BarsList.java
@@ -1,8 +1,11 @@
 package com.example.thebarapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BarsList extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class BarsList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bars_list);
+
+        CardView car = (CardView) findViewById(R.id.card1);
+        car.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 Intent obIntent = new Intent(getApplicationContext(), OldBroadway.class);
+                 startActivity(obIntent);
+            }
+        });
     }
 }
