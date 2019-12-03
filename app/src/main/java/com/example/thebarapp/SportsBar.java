15f4d19a3c395a67a6098f2c87f02d645856d87a
Taylor Schmidt <taylor.n.schmidt@ndsu.edu>
2019-12-03 15:04:57 -0600

    Changed sports bar to use heroku backend, and display reviews

--- app/src/main/java/com/example/thebarapp/SportsBar.java
+++ app/src/main/java/com/example/thebarapp/SportsBar.java
@@ -24,11 +24,14 @@ public class SportsBar extends AppCompatActivity {
     private RatingBar drinksRate;
     private RatingBar atmosphereRate;
     private RatingBar overallRate;
+    private TextView textData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sports_bar);
+        textData = (TextView)findViewById(R.id.commentTxt);
+        textData.setText("Reviews:\n");
         foodRate = findViewById(R.id.foodRate);
         drinksRate = findViewById(R.id.drinkRate);
         atmosphereRate = findViewById(R.id.atmosphereRate);
@@ -44,29 +47,50 @@ public class SportsBar extends AppCompatActivity {
             }
         });
 
-        String url = "http://10.0.2.2:8000/api/reviews/?Bar=The+Sports+Bar";
+        // DB url
+        String url = "http://ndsucsci415.herokuapp.com/api/reviews/?Bar=Sports+Bar";
 
         //Craft the request we send to the backend
+        //Determine what to do with response
         JsonArrayRequest jsonObjectRequest = new JsonArrayRequest
                 (url, new Response.Listener<JSONArray>() {
-                    // This is where the logic goes for after you recieve a response
+
+                    // This is where the logic goes for after you receive a response
                     public void onResponse(JSONArray response) {
                         float overallRating = 0;
                         float drinksRating = 0;
                         float foodRating = 0;
                         float atmosphereRating = 0;
+                        String comment;
 
-                        // Loop through arrays and calculate
+                        // Loop through Response reviews
                         for (int i = 0; i < response.length(); i++) {
                             try {
+                                //Cast to JSONObj
                                 JSONObject row = response.getJSONObject(i);
+
+                                //Get values from Obj
+                                float currentDrinkRating = row.getInt("Drinks");
+                                float currentFoodRating = row.getInt("Food");
+                                float currentAtmosphereRating = row.getInt("Atmosphere");
+                                comment = row.getString("Comment");
+
+
                                 //Sum up stars for each review
-                                drinksRating += row.getInt("Drinks");
-                                foodRating += row.getInt("Food");
-                                atmosphereRating += row.getInt("Atmosphere");
+                                drinksRating += currentDrinkRating;
+                                foodRating += currentFoodRating;
+                                atmosphereRating += currentAtmosphereRating;
+
+                                //Format each review to display
+                                textData.append("---------------------------------------------------");
+                                textData.append("\nFood: " + currentFoodRating + "\n");
+                                textData.append("Drinks: " + currentDrinkRating + "\n");
+                                textData.append("Atmosphere: " + currentAtmosphereRating + "\n");
+                                textData.append("Comment: " +  comment + "\n");
                             }
                             catch (JSONException e){}
                         }
+
                         //Divide by total number of reviews to get average
                         drinksRating = drinksRating / response.length();
                         foodRating = foodRating / response.length();
